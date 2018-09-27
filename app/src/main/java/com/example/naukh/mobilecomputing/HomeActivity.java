76b4c36ef1b15e58b2335d2ee6aff4c155444f51
aroobaanjum43 <aroobaanjum98@yahoo.com>
2018-09-28 00:50:31 +0500

    Initial Repository Commit

---
+++ app/src/main/java/com/example/naukh/mobilecomputing/HomeActivity.java
@@ -0,0 +1,32 @@
+package com.example.naukh.mobilecomputing;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    TextView tvHelloWorld;
+    Context mContext;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        mContext = this;
+        tvHelloWorld = findViewById(R.id.tv_hello_world);
+        tvHelloWorld.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Text view Hello World Click Listener
+                Intent intent=new Intent(mContext,UniversityActivity.class);
+                intent.putExtra("message", "I am coming from home");
+                startActivity(intent);
+
+
+            }
+        });
+    }
+}
